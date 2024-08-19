7da30999e1dddeed08408db50b5413eff1319f33
Ivan Alimin <aliminivan1990@gmail.com>
2024-08-19 15:33:46 +0300

    Maven practice
    - add dependency for GeometryUtils 1.0.0-SNAPSHOT, modify GeometryAppDemo
    with using GeometryUtils methods

--- src/main/java/com/ivanalimin/GeometryAppDemo.java
+++ src/main/java/com/ivanalimin/GeometryAppDemo.java
@@ -1,6 +1,7 @@
 package com.ivanalimin;
 
 import com.ivanalimin.geometry.Circle;
+import com.ivanalimin.geometry.GeometryUtils;
 import com.ivanalimin.geometry.Rectangle;
 import com.ivanalimin.geometry.Triangle;
 
@@ -12,7 +13,7 @@ public class GeometryAppDemo {
     public static void main(String[] args) {
         Circle circle = new Circle(5);
         Rectangle rectangle = new Rectangle(4, 6);
-        Triangle triangle = new Triangle(1, 1, 10);
+        Triangle triangle = new Triangle(3, 4, 5);
 
         System.out.println("Circle area: " + circle.getArea());
         System.out.println("Circle perimeter: " + circle.getPerimeter());
@@ -20,5 +21,12 @@ public class GeometryAppDemo {
         System.out.println("Rectangle perimeter: " + rectangle.getPerimeter());
         System.out.println("Triangle area: " + triangle.getArea());
         System.out.println("Triangle perimeter: " + triangle.getPerimeter());
+
+        double areaInSquareFeet = 100;
+        double areaInSquareMeters = GeometryUtils.convertToSquareMeters(areaInSquareFeet);
+        System.out.println("Area in square meters: " + areaInSquareMeters);
+
+        boolean result = GeometryUtils.areAreasEqual(circle.getArea(), new Circle(5).getArea());
+        System.out.println(result);
     }
 }
